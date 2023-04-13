395b7995a71f12eda339937e1a8f1e126b3f0bb6
rinsham10 <126716201+rinsham10@users.noreply.github.com>
2023-04-13 20:44:34 +0530

    Update 8prime.sh

--- 8prime.sh
+++ 8prime.sh
@@ -1,3 +1,35 @@
+echo "Enter a limit"
+read limit
+echo "prime numbers upto $limit are :"
+echo "1"
+i=2
+while [ $i -le $limit ]
+do
+ flag=1
+ j=2 
+ while [ $j -lt $i ]
+ do
+   rem=$(( $i % $j ))
+  if [ $rem -eq 0 ]
+  then
+    flag=0
+    break
+  fi
+ j=$(( $j+1 ))
+ done
+ if [ $flag -eq 1 ]
+ then
+    echo "$i"
+ fi
+i=$(( $i+1 ))
+done
+
+
+
+
+
+
+
 echo enter a limit
 read limit
 n=2
