5b7fb9a1bd3b6a0e67e5b76a9c299677e41394e7
rinsham10 <126716201+rinsham10@users.noreply.github.com>
2023-04-13 22:39:39 +0530

    Update 23vowels.sh

--- 23vowels.sh
+++ 23vowels.sh
@@ -1,6 +1,6 @@
 echo "enter string:"
 read string
-vow=$(echo $string | grep -o "[aeiou]" | wc --lines)
+vow=$(echo $string | grep -o -i "[aeiou]" | wc --lines)
 cons=$(echo $string | grep -o -i "[bcdfghjklmnpqrstvwxyz]" | wc --lines)
 num=$(echo $string | grep -o -i "[0-9]" | wc --lines)
 echo "total vowels: $vow"
