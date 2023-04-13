deac0b4ad8040d86311a16e953ed8d7aafc1a82c
rinsham10 <126716201+rinsham10@users.noreply.github.com>
2023-04-13 20:10:54 +0530

    Update 5mean.sh

--- 5mean.sh
+++ 5mean.sh
@@ -1,3 +1,19 @@
+echo "Enter three integers with space between"
+read a b c
+sum=`expr $a + $b + $c`
+mean=`expr $sum / 3`
+aa=$((($a - $mean) * ($a - $mean)))
+bb=$((($b - $mean) * ($b - $mean)))
+cc=$((($c - $mean) * ($c - $mean)))
+sd=$( echo "sqrt(($aa + $bb + $cc) / 3)" | bc -l )
+echo "sum=$sum"
+echo "mean=$mean"
+echo "Sd=$sd"
+
+
+
+
+2nd type
 echo "first num"
 read a
 echo "second num"
@@ -11,4 +27,4 @@ r=`expr $c - $m`
 d=$(( ($p * $p + $q * $q + $r * $r) / 3))
 w=$(echo "sqrt ($d )" | bc)
 echo "mean is $m"
-echo "std dev $w
+echo "std dev $w"
