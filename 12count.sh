ff37e9188eb8dfc17604a5b23aef331adaf1dd82
rinsham10 <126716201+rinsham10@users.noreply.github.com>
2023-04-13 21:15:12 +0530

    Update 12count.sh

--- 12count.sh
+++ 12count.sh
@@ -3,6 +3,6 @@ read file
 w=`cat $file | wc -w`
 c=`cat $file | wc -c`
 l=`grep -c "." $file`
-echo number of charecters in $file is $c
+echo number of characters in $file is $c
 echo number of word in $file is $w
 echo number of lines in $file is $l
